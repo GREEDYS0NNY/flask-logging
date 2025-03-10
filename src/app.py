c47bf25986bbf84f30f137a42ef661b2443464c7
ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ <116402204+GREEDYS0NNY@users.noreply.github.com>
2025-03-10 21:42:45 +0000

    Minor changes

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.navigation import navigation
+from routes.authentication import authentication
+from routes.settings import settings
+from routes.reports import reports
+from elasticsearch import Elasticsearch
+
+
+def create_app():
+    app = Flask(__name__)
+
+    CORS(app)
+
+    elasticsearch_client = Elasticsearch(hosts=["http://elasticsearch:9200/"])
+    app.elasticsearch_client = elasticsearch_client
+
+    app.register_blueprint(navigation, url_prefix="/logging/navigation")
+    app.register_blueprint(authentication, url_prefix="/logging/authentication")
+    app.register_blueprint(settings, url_prefix="/logging/settings")
+    app.register_blueprint(reports, url_prefix="/logging/reports")
+
+    return app
+    
+    
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
+    
